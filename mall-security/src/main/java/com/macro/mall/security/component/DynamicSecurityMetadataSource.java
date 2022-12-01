61ab10f0254b0e51597f485efe290ae960b3255e
yuanfacheng <yuanfacheng7032@163.com>
2022-12-01 14:14:34 +0800

    PostConstruct

--- mall-security/src/main/java/com/macro/mall/security/component/DynamicSecurityMetadataSource.java
+++ mall-security/src/main/java/com/macro/mall/security/component/DynamicSecurityMetadataSource.java
@@ -21,6 +21,15 @@ public class DynamicSecurityMetadataSource implements FilterInvocationSecurityMe
     @Autowired
     private DynamicSecurityService dynamicSecurityService;
 
+	/**
+	 * 作用：
+	 * @PostConstruct注解的方法在项目启动的时候执行这个方法，也可以理解为在spring容器启动的时候执行，可作为一些数据的常规化加载，比如数据字典之类的。
+	 *
+	 * 执行顺序：
+	 * 其实从依赖注入的字面意思就可以知道，要将对象p注入到对象a，那么首先就必须得生成对象a和对象p，才能执行注入。所以，如果一个类A中有个成员变量p被@Autowried注解，那么@Autowired注入是发生在A的构造方法执行完之后的。
+	 *
+	 * 如果想在生成对象时完成某些初始化操作，而偏偏这些初始化操作又依赖于依赖注入，那么久无法在构造函数中实现。为此，可以使用@PostConstruct注解一个方法来完成初始化，@PostConstruct注解的方法将会在依赖注入完成后被自动调用。
+	 */
     @PostConstruct
     public void loadDataSource() {
         configAttributeMap = dynamicSecurityService.loadDataSource();
