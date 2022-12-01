c2de80f5ad4d998f589599c49c6f640b5c41d583
yuanfacheng <yuanfacheng7032@163.com>
2022-12-01 14:17:58 +0800

    切面注释

--- mall-security/src/main/java/com/macro/mall/security/aspect/RedisCacheAspect.java
+++ mall-security/src/main/java/com/macro/mall/security/aspect/RedisCacheAspect.java
@@ -24,10 +24,19 @@ import java.lang.reflect.Method;
 public class RedisCacheAspect {
     private static final Logger LOGGER = LoggerFactory.getLogger(RedisCacheAspect.class);
 
+	/**
+	 * 切点，表示包路径下以CacheService结尾的类都是切点目标
+	 */
     @Pointcut("execution(public * com.macro.mall.portal.service.*CacheService.*(..)) || execution(public * com.macro.mall.service.*CacheService.*(..))")
-    public void cacheAspect() {
+	public void cacheAspect() {
     }
 
+	/**
+	 * 环绕执行，表示在目标方法执行前和执行后分别执行一些代码
+	 * @param joinPoint
+	 * @return
+	 * @throws Throwable
+	 */
     @Around("cacheAspect()")
     public Object doAround(ProceedingJoinPoint joinPoint) throws Throwable {
         Signature signature = joinPoint.getSignature();
@@ -35,7 +44,9 @@ public class RedisCacheAspect {
         Method method = methodSignature.getMethod();
         Object result = null;
         try {
+			/***********目标方法执行前*********/
             result = joinPoint.proceed();
+			/***********目标方法执行后*********/
         } catch (Throwable throwable) {
             //有CacheException注解的方法需要抛出异常
             if (method.isAnnotationPresent(CacheException.class)) {
