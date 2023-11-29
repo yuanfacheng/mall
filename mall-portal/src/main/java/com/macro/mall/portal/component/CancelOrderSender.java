0a43228e21d5707928272d7366a3e93c3b117f98
yuanfacheng <yuanfacheng7032@163.com>
2023-11-29 16:12:36 +0800

    放开url，消费者监听mall.order.cancel.ttl队列，用postman调用

--- mall-portal/src/main/java/com/macro/mall/portal/component/CancelOrderSender.java
+++ mall-portal/src/main/java/com/macro/mall/portal/component/CancelOrderSender.java
@@ -32,4 +32,18 @@ public class CancelOrderSender {
         });
         LOGGER.info("send orderId:{}",orderId);
     }
+
+
+	public void sendMessage1(Long orderId,final long delayTimes){
+		//给延迟队列发送消息
+		amqpTemplate.convertAndSend(QueueEnum.QUEUE_TTL_ORDER_CANCEL.getExchange(), QueueEnum.QUEUE_TTL_ORDER_CANCEL.getRouteKey(), orderId, new MessagePostProcessor() {
+			@Override
+			public Message postProcessMessage(Message message) throws AmqpException {
+				//给消息设置延迟毫秒值
+				message.getMessageProperties().setExpiration(String.valueOf(delayTimes));
+				return message;
+			}
+		});
+		LOGGER.info("send orderId:{}",orderId);
+	}
 }
